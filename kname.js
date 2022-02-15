59854b7e5ef766ad1575aeec9557daa33fb52017
tool2000 <shareit@hanmail.net>
2022-02-15 11:00:42 +0900

    random number generating

--- kname.js
+++ kname.js
@@ -1,4 +1,6 @@
 // 한국어 예쁜 이름 짓기 앱
 const kname = [ '현빈', '민서', '현준', '수정', '수민' ]
-console.log(kname[0])
+let index = Math.floor(Math.random() * 5)
+console.log( index )
+console.log(kname[index])
 
